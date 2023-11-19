e66c9752bd87b952185d0d8dc3339e25f9033f5f
BS1207 <refatm452@gmail.com>
2023-11-19 23:03:54 +0600

    Routing implemented

---
+++ Routing/EmitLogDirect/Program.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+internal class EmitLogDirectPublisher
+{
+    private static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory { HostName = "localhost" };
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+
+        channel.ExchangeDeclare(exchange: "direct_logs", type: ExchangeType.Direct);
+
+        var severity = (args.Length > 0) ? args[0] : "info";
+        var message = (args.Length > 1)
+                      ? string.Join(" ", args.Skip(1).ToArray())
+                      : "Hello World!";
+        var body = Encoding.UTF8.GetBytes(message);
+        channel.BasicPublish(exchange: "direct_logs",
+                             routingKey: severity,
+                             basicProperties: null,
+                             body: body);
+        Console.WriteLine($" [x] Sent '{severity}':'{message}'");
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+}
